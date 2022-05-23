b7629efb2f9ad107ba6055a2ec54c707702013c2
Ben Life <77246839+benlife5@users.noreply.github.com>
2022-05-23 15:14:13 -0400

    wrote basic react structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import Response from "./Response.js"
 
 function App() {
+
+  const responses = [
+    {id: 1, responseText: "Text", upvotes: 10},
+    {id: 2, responseText: "Text2", upvotes: 20}
+  ]
+
+  const upvote = (responseID) => {
+
+  }
+
   return (
     <div className="App">
+      <h1>Favorite Ice Cream Flavor?</h1>
+      <input type="text" />
+      <input type="submit" />
+      {responses.map((response) => <Response id={response.id} responseText={response.responseText} upvotes={response.upvotes} upvote={upvote}/> )}
     </div>
   );
 }
---
+++ src/Response.js
@@ -0,0 +1,12 @@
+
+function Response(props) {
+  return (
+    <div className="response">
+      <h3>{props.responseText}</h3>
+      <p>{props.upvotes} upvotes</p>
+      <input type="submit" onClick={() => props.upvote(props.id)} value="Upvote" />
+    </div>
+  );
+}
+
+export default Response;
